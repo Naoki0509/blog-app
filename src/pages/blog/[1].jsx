5e650b60473730d47dee7949524dff69ff884f1f
Naoki0509 <70335387+Naoki0509@users.noreply.github.com>
2022-02-07 01:33:38 +0900

    blogの子ページの作成

---
+++ src/pages/blog/[1].jsx
@@ -0,0 +1,9 @@
+import styles from "src/styles/Home.module.css";
+
+export default function Home() {
+	return (
+		<div className="flex items-center justify-center">
+			<div className=""></div>
+		</div>
+	);
+}
