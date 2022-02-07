d7e15e6298593e7acb2dd1940a05458ddfb0fa58
Naoki0509 <70335387+Naoki0509@users.noreply.github.com>
2022-02-07 10:11:40 +0900

    💄 スタイルの追加

--- src/COmponents/Header/index.jsx
+++ src/COmponents/Header/index.jsx
@@ -2,18 +2,20 @@ import Link from "next/link";
 
 export function Header() {
 	return (
-		<div className="flex items-center justify-start mt-6">
-			<div className="text-4xl font-serif font-bold">N Blog</div>
-			<div className="ml-8">
+		<div className="flex fixed items-center justify-start mt-4">
+			<Link href="/">
+				<div className="text-4xl font-serif font-bold">N Blog</div>
+			</Link>
+			<div className="ml-12">
 				<ul className="flex justify-start space-x-6 font-bold font-serif text-2xl">
 					<li>
 						<Link href="/">
-							<a>Home</a>
+							<a className="hover:text-blue-400">Home</a>
 						</Link>
 					</li>
 					<li>
 						<Link href="/blog">
-							<a>Blog</a>
+							<a className="hover:text-blue-400">Blog</a>
 						</Link>
 					</li>
 				</ul>
