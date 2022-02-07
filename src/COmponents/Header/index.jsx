a7614347abef8a27de0e818e92950929fbfbd92d
Naoki0509 <70335387+Naoki0509@users.noreply.github.com>
2022-02-07 10:17:34 +0900

    🔥

--- src/COmponents/Header/index.jsx
+++ src/COmponents/Header/index.jsx
@@ -3,9 +3,7 @@ import Link from "next/link";
 export function Header() {
 	return (
 		<div className="flex fixed items-center justify-start mt-4">
-			<Link href="/">
-				<div className="text-4xl font-serif font-bold">N Blog</div>
-			</Link>
+			<div className="text-4xl font-serif font-bold">N Blog</div>
 			<div className="ml-12">
 				<ul className="flex justify-start space-x-6 font-bold font-serif text-2xl">
 					<li>
