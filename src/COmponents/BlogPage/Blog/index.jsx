b7b6b72c009b1187d3f4f6b154bd3a8071a6cdc4
Naoki0509 <70335387+Naoki0509@users.noreply.github.com>
2022-02-13 14:06:37 +0900

    💡

--- src/COmponents/BlogPage/Blog/index.jsx
+++ src/COmponents/BlogPage/Blog/index.jsx
@@ -3,8 +3,29 @@ import styles from "src/styles/Home.module.css";
 export function Blog() {
 	return (
 		<main className={styles.main}>
-			<div className="text-5xl font-bold font-serif">Blog</div>
-			<div className="text-left texxt-2xl text-gray-500">
+			<div>
+				<div className="snap-y snap-mandatory h-screen w-screen overflow-scroll">
+					<div className="snap-start flex items-center justify-center h-screen w-screen bg-orange-600 text-5xl font-bold font-serif">
+						Blog
+					</div>
+					<div className="snap-start flex items-center justify-center h-screen w-screen bg-blue-600 text-5xl font-bold font-serif">
+						Blog
+					</div>
+					<div className="snap-start flex items-center justify-center h-screen w-screen bg-gray-600 text-5xl font-bold font-serif">
+						Blog
+					</div>
+					<div className="snap-start flex items-center justify-center h-screen w-screen bg-green-600 text-5xl font-bold font-serif">
+						Blog
+					</div>
+					<div className="snap-start flex items-center justify-center h-screen w-screen bg-orange-600 text-5xl font-bold font-serif">
+						Blog
+					</div>
+					<div className="snap-start flex items-center justify-center h-screen w-screen bg-blue-600 text-5xl font-bold font-serif">
+						Blog
+					</div>
+				</div>
+			</div>
+			{/* <div className="text-left texxt-2xl text-gray-500">
 				<p>はじめまして。Naokiです。</p>
 				<p>私は現在、フロントエンドエンジニアを目指しています。</p>
 				<p>さて、僕はこのブログを通して、このブログを見てくださった方に、</p>
@@ -14,7 +35,7 @@ export function Blog() {
 				<p>
 					まぁ日々感じた疑問だったり、思い描いたことをここに綴っていけたらなと思うのでどうぞよろしくお願いいたします。
 				</p>
-			</div>
+			</div> */}
 		</main>
 	);
 }
