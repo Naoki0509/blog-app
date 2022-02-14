0e50b419b6b1f9d370ac59099ea97a15b6aac4ad
Naoki0509 <70335387+Naoki0509@users.noreply.github.com>
2022-02-14 15:28:52 +0900

    💡 Imageコンポーネントの追加

--- src/COmponents/BlogPage/Blog/index.jsx
+++ src/COmponents/BlogPage/Blog/index.jsx
@@ -1,41 +1,39 @@
 import styles from "src/styles/Home.module.css";
+import Image from "next/image";
+import ProfilePicture from "src/public/132046306626613212998.jpeg";
 
 export function Blog() {
 	return (
 		<main className={styles.main}>
-			<div>
-				<div className="snap-y snap-mandatory h-screen w-screen overflow-scroll">
-					<div className="snap-start flex items-center justify-center h-screen w-screen bg-orange-600 text-5xl font-bold font-serif">
-						Blog
-					</div>
-					<div className="snap-start flex items-center justify-center h-screen w-screen bg-blue-600 text-5xl font-bold font-serif">
-						Blog
-					</div>
-					<div className="snap-start flex items-center justify-center h-screen w-screen bg-gray-600 text-5xl font-bold font-serif">
-						Blog
-					</div>
-					<div className="snap-start flex items-center justify-center h-screen w-screen bg-green-600 text-5xl font-bold font-serif">
-						Blog
-					</div>
-					<div className="snap-start flex items-center justify-center h-screen w-screen bg-orange-600 text-5xl font-bold font-serif">
-						Blog
-					</div>
-					<div className="snap-start flex items-center justify-center h-screen w-screen bg-blue-600 text-5xl font-bold font-serif">
-						Blog
-					</div>
+			<div className="lg:w-1/2 p-12 shadow-xl border-2 border-gray-200">
+				<div className="pb-10 flex items-center justify-center">
+					<Image
+						src={ProfilePicture}
+						alt="Profile"
+						layout="fixed"
+						width="150px"
+						height="150px"
+						className="rounded-full"
+					/>
+				</div>
+				<div className="text-left texxt-xl text-gray-500">
+					<p>はじめまして。Naokiです。</p>
+					<p>私は現在、フロントエンドエンジニアを目指しています。</p>
+					<p>さて、僕はこのブログを通して、このブログを見てくださった方に、</p>
+					<p>
+						次の日により良くなるように日々のアドバイスのようなものを発信していけたらなと思っていますが、
+					</p>
+					<p>
+						まぁ日々感じた疑問だったり、思い描いたことをここに綴っていけたらなと思うのでどうぞよろしくお願いいたします。
+					</p>
 				</div>
 			</div>
-			{/* <div className="text-left texxt-2xl text-gray-500">
-				<p>はじめまして。Naokiです。</p>
-				<p>私は現在、フロントエンドエンジニアを目指しています。</p>
-				<p>さて、僕はこのブログを通して、このブログを見てくださった方に、</p>
-				<p>
-					次の日により良くなるように日々のアドバイスのようなものを発信していけたらなと思っていますが、
-				</p>
-				<p>
-					まぁ日々感じた疑問だったり、思い描いたことをここに綴っていけたらなと思うのでどうぞよろしくお願いいたします。
-				</p>
-			</div> */}
+			<div className="flex-cokl items-center justify-center">
+				<div className="p-12">1.はじめまして</div>
+				<div className="p-12">1.はじめまして</div>
+				<div className="p-12">1.はじめまして</div>
+				<div className="p-12">1.はじめまして</div>
+			</div>
 		</main>
 	);
 }
