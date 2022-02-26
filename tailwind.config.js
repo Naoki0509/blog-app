519f951860f55bf3bb8b2c1b4bc05b55014478b3
Naoki0509 <70335387+Naoki0509@users.noreply.github.com>
2022-02-26 21:10:11 +0900

    📦 パッケージの追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
 	content: [
 		"src/pages/**/*.{js,ts,jsx,tsx}",
 		"src/components/**/*.{js,ts,jsx,tsx}",
+		"node_modules/flowbite/**/*.js",
 	],
 	theme: {
 		extend: {
@@ -12,5 +13,5 @@ module.exports = {
 			},
 		},
 	},
-	plugins: [],
+	plugins: [require("flowbite/plugin")],
 };
