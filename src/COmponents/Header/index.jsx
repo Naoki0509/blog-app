e0216a25cfcf944938b3cd77e7a27392c9a95a20
Naoki0509 <70335387+Naoki0509@users.noreply.github.com>
2022-03-10 18:21:38 +0900

    💄 スタイルの追加

--- src/COmponents/Header/index.jsx
+++ src/COmponents/Header/index.jsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export const Header = () => {
 	return (
-		<div className="flex fixed items-center justify-start  pt-4">
-			<div className="text-4xl font-serif font-bold">N Blog</div>
-			<div className="ml-12">
-				<ul className="md:flex hidden justify-start space-x-6 font-bold font-serif text-2xl">
+		<div className="flex lg:justify-start justify-center items-center pt-4">
+			<p className="text-4xl font-serif font-bold">N Blog</p>
+			<div className="lg:ml-12">
+				<ul className="lg:flex hidden justify-start space-x-6 font-bold font-serif text-2xl">
 					<li>
 						<Link href="/">
 							<a className="hover:text-blue-400">Home</a>
@@ -18,6 +18,7 @@ export const Header = () => {
 					</li>
 				</ul>
 			</div>
+			<div className="lg:hidden absolute bottom-0 right-0 w-12 h-12 bg-blue-100"></div>
 		</div>
 	);
 };
