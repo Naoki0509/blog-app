b1e8326062f75fb3e25a8ebaabddbea3d1ddf617
Naoki0509 <70335387+Naoki0509@users.noreply.github.com>
2022-03-10 18:22:32 +0900

    💡 ダークモード対応テスト中

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import { Footer } from "src/COmponents/Footer";
 import { Header } from "src/COmponents/Header";
 import { Profile } from "src/COmponents/Profile";
+import { Theme } from "src/COmponents/Theme";
 
 export default function Blog() {
 	return (
@@ -12,6 +13,8 @@ export default function Blog() {
 			<Header />
 			<div className="min-h-screen p-main flex-col flex-1 items-center justify-center">
 				<Profile />
+				<div></div>
+				{/* <Theme /> */}
 			</div>
 			<Footer />
 		</div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,10 @@
-import Head from "next/head";
 import Image from "next/image";
+import Head from "next/head";
 import { useState, useEffect, useCallback } from "react";
 import { Footer } from "src/COmponents/Footer";
 import { Header } from "src/COmponents/Header";
 import { Profile } from "src/COmponents/Profile";
+import { Theme } from "src/COmponents/Theme";
 
 export default function Home() {
 	return (
@@ -14,6 +15,10 @@ export default function Home() {
 			<Header />
 			<div className="min-h-screen p-main flex-1 flex-col justify-center items-center ">
 				<Profile />
+				{/* <Theme />  */}
+				<div>
+					<p>Project</p>
+				</div>
 			</div>
 			<Footer />
 		</div>
